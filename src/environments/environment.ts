a8eeea2ad79e003f484481c2d93779daad83ebea
tronxi <sergio.gsanchez97@gmail.com>
2019-11-24 17:02:05 +0100

    new route with nginx

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   environment: 'default',
-  url: 'http://raspberrytronxi2.ddns.net:8079'
+  url: 'http://raspberrytronxi3.ddns.net/framework-educativo-back'
 };
